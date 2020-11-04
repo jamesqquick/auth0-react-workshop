890f7e1fd49f20bebbc2983afeaf45528bc6ed7a
James Quick <james.q.quick@outlook.com>
2020-11-04 15:38:59 -0600

    protecting profile route

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -4,6 +4,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 import { NavBar, Footer, Loading } from './components';
 import { Home, Profile, ExternalApi } from './views';
+import ProtectedRoute from './auth/protected-route';
 
 import './app.css';
 
@@ -20,8 +21,11 @@ const App = () => {
             <div className="container flex-grow-1">
                 <Switch>
                     <Route path="/" exact component={Home} />
-                    <Route path="/profile" component={Profile} />
-                    <Route path="/external-api" component={ExternalApi} />
+                    <ProtectedRoute path="/profile" component={Profile} />
+                    <ProtectedRoute
+                        path="/external-api"
+                        component={ExternalApi}
+                    />
                 </Switch>
             </div>
             <Footer />
