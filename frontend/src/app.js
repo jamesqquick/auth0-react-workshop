341b57868db2403dc6642fea6d820b005e112e5c
James Quick <james.q.quick@outlook.com>
2020-11-04 14:57:15 -0600

    Added authentication buttons and nav

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,27 +1,32 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
-import { NavBar, Footer } from "./components";
-import { Home, Profile, ExternalApi } from "./views";
+import { NavBar, Footer, Loading } from './components';
+import { Home, Profile, ExternalApi } from './views';
 
-import "./app.css";
+import './app.css';
 
 const App = () => {
-  return (
-    <div id="app" className="d-flex flex-column h-100">
-      <NavBar />
-      <div className="container flex-grow-1">
-        <div className="mt-5">
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/external-api" component={ExternalApi} />
-          </Switch>
+    const { isLoading } = useAuth0();
+
+    if (isLoading) {
+        return <Loading />;
+    }
+
+    return (
+        <div id="app" className="d-flex flex-column h-100">
+            <NavBar />
+            <div className="container flex-grow-1">
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/external-api" component={ExternalApi} />
+                </Switch>
+            </div>
+            <Footer />
         </div>
-      </div>
-      <Footer />
-    </div>
-  );
+    );
 };
 
 export default App;
