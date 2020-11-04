45040b48b7aedc0fc0db2fa3e0655f61b3099b71
James Quick <james.q.quick@outlook.com>
2020-11-04 15:37:30 -0600

    Added profile information

--- frontend/src/views/profile.js
+++ frontend/src/views/profile.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React from 'react';
+
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Profile = () => {
-  return (
-    <div>
-      <h1>Profile</h1>
-      <p>
-        You use will use an ID Token to get the profile information of a
-        logged-in user.
-        <br />
-        <strong>This route should be protected</strong>.
-      </p>
-    </div>
-  );
+    const { user } = useAuth0();
+    const { name, picture, email } = user;
+
+    return (
+        <div>
+            <div className="row align-items-center profile-header">
+                <div className="col-md-2 mb-3">
+                    <img
+                        src={picture}
+                        alt="Profile"
+                        className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+                    />
+                </div>
+                <div className="col-md text-center text-md-left">
+                    <h2>{name}</h2>
+                    <p className="lead text-muted">{email}</p>
+                </div>
+            </div>
+            <div className="row">
+                <pre className="col-12 text-light bg-dark p-4">
+                    {JSON.stringify(user, null, 2)}
+                </pre>
+            </div>
+        </div>
+    );
 };
 
 export default Profile;
