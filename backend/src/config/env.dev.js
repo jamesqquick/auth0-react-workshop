5f456517efce98e83eb6ccddd7632cbd027f4e51
James Quick <james.q.quick@outlook.com>
2020-11-12 10:23:10 -0600

    Domain insteAD OF ISSUEr

--- backend/src/config/env.dev.js
+++ backend/src/config/env.dev.js
@@ -1,4 +1,4 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
 dotenv.config();
 
@@ -8,35 +8,77 @@ const serverPort = process.env.SERVER_PORT;
 const clientOriginUrl = process.env.CLIENT_ORIGIN_URL;
 
 if (!audience) {
-  throw new Error(
-    ".env is missing the definition of an AUTH0_AUDIENCE environmental variable"
-  );
+    throw new Error(
+        '.env is missing the definition of an AUTH0_AUDIENCE environmental variable'
+    );
 }
 
 if (!issuerUrl) {
-  throw new Error(
-    ".env is missing the definition of an AUTH0_ISSUER environmental variable"
-  );
+    throw new Error(
+        '.env is missing the definition of an AUTH0_ISSUER environmental variable'
+    );
 }
 
 if (!serverPort) {
-  throw new Error(
-    ".env is missing the definition of a API_PORT environmental variable"
-  );
+    throw new Error(
+        '.env is missing the definition of a API_PORT environmental variable'
+    );
 }
 
 if (!clientOriginUrl) {
-  throw new Error(
-    ".env is missing the definition of a APP_ORIGIN environmental variable"
-  );
+    throw new Error(
+        '.env is missing the definition of a APP_ORIGIN environmental variable'
+    );
 }
 
-const clientOrigins = ["http://localhost:4040"];
+const clientOrigins = ['http://localhost:4040'];
 
 module.exports = {
-  audience,
-  issuerUrl,
-  serverPort,
-  clientOriginUrl,
-  clientOrigins,
+    audience,
+    issuerUrl,
+    serverPort,
+    clientOriginUrl,
+    clientOrigins,
+};
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const audience = process.env.AUTH0_AUDIENCE;
+const domain = process.env.AUTH0_DOMAIN;
+const serverPort = process.env.SERVER_PORT;
+const clientOriginUrl = process.env.CLIENT_ORIGIN_URL;
+
+if (!audience) {
+    throw new Error(
+        '.env is missing the definition of an AUTH0_AUDIENCE environmental variable'
+    );
+}
+
+if (!domain) {
+    throw new Error(
+        '.env is missing the definition of an AUTH0_DOMAIN environmental variable'
+    );
+}
+
+if (!serverPort) {
+    throw new Error(
+        '.env is missing the definition of a API_PORT environmental variable'
+    );
+}
+
+if (!clientOriginUrl) {
+    throw new Error(
+        '.env is missing the definition of a APP_ORIGIN environmental variable'
+    );
+}
+
+const clientOrigins = ['http://localhost:4040'];
+
+module.exports = {
+    audience,
+    domain,
+    serverPort,
+    clientOriginUrl,
+    clientOrigins,
 };
