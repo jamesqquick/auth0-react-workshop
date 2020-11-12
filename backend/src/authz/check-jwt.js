c8f03c8ddf34d86701c65509e18cfe2980fc7860
James Quick <james.q.quick@outlook.com>
2020-11-12 10:27:17 -0600

    Domain instead of issuer

--- backend/src/authz/check-jwt.js
+++ backend/src/authz/check-jwt.js
@@ -1,20 +1,20 @@
-const jwt = require("express-jwt");
-const jwksRsa = require("jwks-rsa");
-const { issuerUrl, audience } = require("../config/env.dev");
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+const { domain, audience } = require('../config/env.dev');
 
 const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `${issuerUrl}.well-known/jwks.json`,
-  }),
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${domain}/.well-known/jwks.json`,
+    }),
 
-  audience: audience,
-  issuer: issuerUrl,
-  algorithms: ["RS256"],
+    audience: audience,
+    issuer: `https://${domain}/`,
+    algorithms: ['RS256'],
 });
 
 module.exports = {
-  checkJwt,
+    checkJwt,
 };
--- backend/src/config/env.dev.js
+++ backend/src/config/env.dev.js
@@ -1,42 +1,42 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const audience = process.env.AUTH0_AUDIENCE;
-const issuerUrl = process.env.AUTH0_ISSUER_URL;
+const domain = process.env.AUTH0_DOMAIN;
 const serverPort = process.env.SERVER_PORT;
 const clientOriginUrl = process.env.CLIENT_ORIGIN_URL;
 
 if (!audience) {
-  throw new Error(
-    ".env is missing the definition of an AUTH0_AUDIENCE environmental variable"
-  );
+    throw new Error(
+        '.env is missing the definition of an AUTH0_AUDIENCE environmental variable'
+    );
 }
 
-if (!issuerUrl) {
-  throw new Error(
-    ".env is missing the definition of an AUTH0_ISSUER environmental variable"
-  );
+if (!domain) {
+    throw new Error(
+        '.env is missing the definition of an AUTH0_DOMAIN environmental variable'
+    );
 }
 
 if (!serverPort) {
-  throw new Error(
-    ".env is missing the definition of a API_PORT environmental variable"
-  );
+    throw new Error(
+        '.env is missing the definition of a API_PORT environmental variable'
+    );
 }
 
 if (!clientOriginUrl) {
-  throw new Error(
-    ".env is missing the definition of a APP_ORIGIN environmental variable"
-  );
+    throw new Error(
+        '.env is missing the definition of a APP_ORIGIN environmental variable'
+    );
 }
 
-const clientOrigins = ["http://localhost:4040"];
+const clientOrigins = ['http://localhost:4040'];
 
 module.exports = {
-  audience,
-  issuerUrl,
-  serverPort,
-  clientOriginUrl,
-  clientOrigins,
+    audience,
+    domain,
+    serverPort,
+    clientOriginUrl,
+    clientOrigins,
 };
