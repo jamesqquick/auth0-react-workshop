f12a5e0e2dd1fda80e574db8c64e97e13c08815d
James Quick <james.q.quick@outlook.com>
2020-11-12 10:26:06 -0600

    Domain instead of issuer

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -4,7 +4,6 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 import { NavBar, Footer, Loading } from './components';
 import { Home, Profile, ExternalApi } from './views';
-import ProtectedRoute from './auth/protected-route';
 
 import './app.css';
 
@@ -21,11 +20,8 @@ const App = () => {
             <div className="container flex-grow-1">
                 <Switch>
                     <Route path="/" exact component={Home} />
-                    <ProtectedRoute path="/profile" component={Profile} />
-                    <ProtectedRoute
-                        path="/external-api"
-                        component={ExternalApi}
-                    />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/external-api" component={ExternalApi} />
                 </Switch>
             </div>
             <Footer />
